0c08ff10751c96dbc1bb5d8f5b8626f6cdf84b7a
Ronald Zwiers <Ronald Zwiers>
2016-10-12 21:36:07 +0200

    Laatste update 12-10-2016

--- resources/views/pages/blogpage/blogdetailpage/view.blade.php
+++ resources/views/pages/blogpage/blogdetailpage/view.blade.php
@@ -1,15 +1,36 @@
 <!-- {{$debugpath}} -->
-<div class="blog-wrapper">
+<div class="single-blog-wrapper">
     <article>
-        <figure class="image-wrapper image-2-3">
+        <h2 class="single-blog-title">{{$blog->title}}</h2>
+        <p><small>{{ Lang::get('blogpage.meta-postedby') }} <b>{{$blog->Author->name}}</b> {{ Lang::get('blogpage.meta-time') }} <b>{{date('F d, Y', strtotime($blog->created_at))}}</b></small></p>
+
+        <div class="comp-alinea intro">
+            <p class="intro">{{$blog->content[0]}}</p>
+        </div>    
+
+        <figure class="image-wrapper image-1-2">
             <img class="image" src="{{ asset('img/blog/'.$blog->images[0]->file)}}" alt="Logo {{ $globals->title }}">
         </figure>
 
-        <div class="text">
-            <h2>{{$blog->title}}</h2>
-            <p><small>{{ Lang::get('blogpage.meta-postedby') }} <b>{{$blog->Author->name}}</b> {{ Lang::get('blogpage.meta-time') }} <b>{{date('F d, Y', strtotime($blog->created_at))}}</b></small></p>
-            <p>{{$blog->content[0]}}</p>
+        <div class="comp-alinea">
+            <p>{{$blog->content[1]}}</p>
         </div>
+
+        <div class="comp-alinea">
+            <p>{{$blog->content[2]}}</p>
+        </div>  
+
+        <div class="comp-alinea">
+            <p>{{$blog->content[3]}}</p>
+        </div> 
+
+        <div class="comp-alinea">
+            <p>{{$blog->content[4]}}</p>
+        </div>    
+
+        <figure class="image-wrapper image-1-2">
+            <img class="image" src="{{ asset('img/blog/'.$blog->images[0]->file)}}" alt="Logo {{ $globals->title }}">
+        </figure>                          
     </article>
 
     @if(isset($blog->comments[0]))
