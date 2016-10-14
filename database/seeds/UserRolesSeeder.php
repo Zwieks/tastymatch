2b054608b5f4604909c0323983688e04ca4cb2b3
r.zwiers <r.zwiers@iwink.nl>
2016-10-14 15:28:54 +0200

    13-10-2016 comments update parallax

---
+++ database/seeds/UserRolesSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UserRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        DB::table('roles')->insert([
+        [
+            'name' => 'quest',
+            'description' => null,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ],
+        [
+            'name' => 'entertainer',
+            'description' => null,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ],
+        [
+            'name' => 'organiser',
+            'description' => null,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ],
+        [
+            'name' => 'admin',
+            'description' => null,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ],
+        [
+            'name' => 'blogger',
+            'description' => null,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]]);
+    }
+}
