d7863ef07ef41fb9046eb52200c4bdca650d32b2
r.zwiers <r.zwiers@iwink.nl>
2016-10-26 17:21:37 +0200

    26-10-2016 homepage update

--- app/FoodstandType.php
+++ app/FoodstandType.php
@@ -8,12 +8,16 @@ class FoodstandType extends Model
 {
 
 	/**
+	 * The associated table.
+	 *
+	 */
+	protected $table = 'foodstandtypes';
+
+	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
 	 */
-	protected $table = 'foodstandtypes';
-
 	protected $fillable = [
 		'name', 'name_en', 'description'
 	];
