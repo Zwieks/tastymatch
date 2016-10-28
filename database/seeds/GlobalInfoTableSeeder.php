3edff0172a146b6d102f4df95be07e794af45cd6
r.zwiers <r.zwiers@iwink.nl>
2016-10-28 10:07:04 +0200

    update

---
+++ database/seeds/GlobalInfoTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class GlobalInfoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
