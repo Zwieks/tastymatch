37c518ffb3dbd81f8222bb7b32cd23b6c0b95c25
r.zwiers <r.zwiers@iwink.nl>
2016-10-31 17:39:15 +0100

    31-10-2016

--- resources/views/sidebars/templates/submenu.blade.php
+++ resources/views/sidebars/templates/submenu.blade.php
@@ -1,8 +1,17 @@
 <!-- {{$debugpath}} -->
 @if(Auth::check())
 	<section class="page-submenu">
-		<h2>{{ Lang::get('menus.submenu-title') }}</h2>
+		<h2 class="submenu-title">{{ Lang::get('menus.submenu-title') }}</h2>
 		<ul class="submenu-items">
+			<li>
+				<a class="submenu-item" href="{{ Lang::get('menus.profile-url') }}" data-icon="a">{{ Lang::get('menus.profile') }}</a>
+			</li>
+			<li>
+				<a class="submenu-item" href="{{ Lang::get('menus.profile-url') }}" data-icon="a">{{ Lang::get('menus.profile') }}</a>
+			</li>
+			<li>
+				<a class="submenu-item active" href="{{ Lang::get('menus.profile-url') }}" data-icon="a">{{ Lang::get('menus.profile') }}</a>
+			</li>
 			<li>
 				<a class="submenu-item" href="{{ Lang::get('menus.profile-url') }}" data-icon="a">{{ Lang::get('menus.profile') }}</a>
 			</li>
