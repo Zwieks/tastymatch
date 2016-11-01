99f25613febd8f8f30479ac730cda451d5f5bc67
r.zwiers <r.zwiers@iwink.nl>
2016-11-01 17:28:02 +0100

    01-12-2016

--- resources/views/sidebars/templates/userproducts.blade.php
+++ resources/views/sidebars/templates/userproducts.blade.php
@@ -3,11 +3,13 @@
     <section class="page-userproducts">
         <h2 class="sidebar-title">{{ Lang::get('menus.userproducts-title') }}</h2>
         <ul class="userproducts-items">
-            @foreach($user->foodstands as $foodstand)
-                <li>
-                    <a class="userproducts-item" href="/" data-icon="G"> {{$foodstand->name}}</a>
-                </li>
-            @endforeach
+            @if( !empty($user->foodstands))
+                @foreach($user->foodstands as $foodstand)
+                    <li>
+                        <a class="userproducts-item" href="/" data-icon="G"> {{$foodstand->name}}</a>
+                    </li>
+                @endforeach
+            @endif
         </ul>
     </section>
 @endif
