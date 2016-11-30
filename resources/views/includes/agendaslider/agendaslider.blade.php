9474f5681987d3655188b0d1a2963d1540562998
r.zwiers <r.zwiers@iwink.nl>
2016-11-30 14:10:40 +0100

    30-11-2016

--- resources/views/includes/agendaslider/agendaslider.blade.php
+++ resources/views/includes/agendaslider/agendaslider.blade.php
@@ -1,19 +1,26 @@
 <!-- {{$debugpath}} -->
-<div id="js-agenda-scroller">
-    <section class="page-timeline" id="timeline">
-        <h2>
-            {_ 'Bekijk de planning van het project'}
-            <span class="slider-prev heading-btn" id="js-timeline-prev" data-icon="o"></span>
-            <span class="slider-next heading-btn" id="js-timeline-next" data-icon="m"></span>
-        </h2>
+<section class="page-agendaslider" id="agendaslider">
+    <h2>
+        Aankomende agenda items
+        <span class="slider-prev heading-btn" id="js-timeline-prev" data-icon="T"></span>
+        <span class="slider-next heading-btn" id="js-timeline-next" data-icon="S"></span>
+    </h2>
 
-        <div class="timeline-scroller" id="js-timeline-scroller" data-current-year="{$smarty.now|date_format:'%Y'|escape}">
-            {foreach $timeline_items as $timeline_item}
-            {render object=$timeline_item}
-            {/foreach}
-            {* Add two more items for extra spacing *}
-            <div class="timeline-item last-item"></div>
-            <div class="timeline-item last-item"></div>
-        </div>
-    </section>
-</div>
+    <div id="js-agenda-scroller">
+        @foreach($user->agenda as $item )
+            <article class="timeline-item">
+                <div class="timeline-item-wrapper">
+                    <header>
+                        <h3>{{ $item->name }}</h3>
+                        <p class="date">{{ $item->created_at }}</p>
+                    </header>
+
+                   <p>{{ $item->description }}</p>
+                </div>
+            </article>
+        @endforeach
+
+        <div class="timeline-item last-item"></div>
+        <div class="timeline-item last-item"></div>
+    </div>
+</section>
